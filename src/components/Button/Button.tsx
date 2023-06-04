3ed12b2faeb5431b1218158daed4bf3dd4484e8c
Adrian <adrianmroz82@gmail.com>
2023-06-04 23:31:26 +0200

    button hover idea

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { MouseEvent } from "react";
+
 interface Props {
   text: string;
   variant: "primary" | "secondary";
@@ -5,11 +8,51 @@ interface Props {
 }
 
 const Button = ({ text, variant, fullWidth }: Props) => {
+  const getButtonStyle = () => {
+    if (variant === "primary") {
+      return {
+        backgroundColor: "white",
+        color: "black",
+      };
+    } else if (variant === "secondary") {
+      return {
+        backgroundColor: "black",
+        color: "white",
+      };
+    }
+  };
+
+  const getHoverStyle = () => {
+    if (variant === "primary") {
+      return {
+        backgroundColor: "black",
+        color: "white",
+      };
+    } else if (variant === "secondary") {
+      return {
+        backgroundColor: "white",
+        color: "black",
+      };
+    }
+  };
+
+  const handleMouseEnter = (e: MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+    const hoverStyle = getHoverStyle();
+    target.style.backgroundColor = hoverStyle!.backgroundColor;
+    target.style.color = hoverStyle!.color;
+  };
+
+  const handleMouseLeave = (e: MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+    const buttonStyle = getButtonStyle();
+    target.style.backgroundColor = buttonStyle!.backgroundColor;
+    target.style.color = buttonStyle!.color;
+  };
+
   return (
     <button
       style={{
-        backgroundColor: variant === "primary" ? "white" : "black",
-        color: variant === "primary" ? "black" : "white",
         padding: "10px 20px",
         cursor: "pointer",
         fontSize: "1rem",
@@ -17,10 +60,36 @@ const Button = ({ text, variant, fullWidth }: Props) => {
         width: fullWidth ? "100%" : "auto",
         textTransform: "uppercase",
         letterSpacing: "2px",
-      }}>
+        transition: "background-color 0.5s, color 0.5s", // Add transition property
+
+        ...getButtonStyle(),
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}>
       {text}
     </button>
   );
 };
 
 export default Button;
+
+// const Button = ({ text, variant, fullWidth }: Props) => {
+//   return (
+//     <button
+//       style={{
+//         backgroundColor: variant === "primary" ? "white" : "black",
+//         color: variant === "primary" ? "black" : "white",
+//         padding: "10px 20px",
+//         cursor: "pointer",
+//         fontSize: "1rem",
+//         fontWeight: "bold",
+//         width: fullWidth ? "100%" : "auto",
+//         textTransform: "uppercase",
+//         letterSpacing: "2px",
+//       }}>
+//       {text}
+//     </button>
+//   );
+// };
+
+// export default Button;
