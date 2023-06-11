bfa24271ad23dd30c7abe3732a2aedf3fd25fbd4
Adrian <adrianmroz82@gmail.com>
2023-06-11 17:53:27 +0200

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import "./App.css";
 import data from "./data.json";
 
-import Header from "./components/Header/Header";
-import { createContext, useContext } from "react";
-import FeaturedArtwork from "./components/FeaturedArtwork/FeaturedArtwork";
+import { Header } from "./components/Header/Header";
+import { createContext } from "react";
+import { FeaturedArtwork } from "./components/FeaturedArtwork/FeaturedArtwork";
+import { ArtworkBrowser } from "./components/ArtworkBrowser/ArtworkBrowser";
 
 console.log(data);
 
 export const DataContext = createContext(data);
 
-function App() {
+export const App = () => {
   return (
     <DataContext.Provider value={data}>
       <Header />
       <FeaturedArtwork />
+      <ArtworkBrowser />
     </DataContext.Provider>
   );
-}
-
-export default App;
+};
