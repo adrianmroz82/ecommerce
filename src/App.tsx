32e9fb779b88efe1b1a9792a1c1d2ec4f1d9d0ba
Adrian <adrianmroz82@gmail.com>
2023-06-19 16:12:33 +0200

    add mock firebase

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,40 @@ import "./App.css";
 import data from "./data.json";
 
 import { Header } from "./components/Header/Header";
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { FeaturedArtwork } from "./components/FeaturedArtwork/FeaturedArtwork";
 import { ArtworkBrowser } from "./components/ArtworkBrowser/ArtworkBrowser";
 
-console.log(data);
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoY5CkJuhQF33v0YwfWWIrCVgG1d6HaXg",
+  authDomain: "shop-5c66f.firebaseapp.com",
+  projectId: "shop-5c66f",
+  storageBucket: "shop-5c66f.appspot.com",
+  messagingSenderId: "1096838676439",
+  appId: "1:1096838676439:web:d9adf9e1d712d206a69592",
+  measurementId: "G-Q2D43BQQD7",
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
 
 export const DataContext = createContext(data);
 
 export const App = () => {
+  const productsCollectionRef = collection(db, "products");
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const products = await getDocs(productsCollectionRef);
+      console.log(products.docs.map((doc) => doc.data()));
+    };
+
+    getProducts();
+  });
   return (
     <DataContext.Provider value={data}>
       <Header />
